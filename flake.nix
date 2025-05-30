11412e718c0c30ac225d50298aa9441c3b7633b3
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2025-05-30 09:33:22 +0200

    feat: simplify flakeless install

--- flake.nix
+++ flake.nix
@@ -22,17 +22,9 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
-          sources = builtins.fromJSON (builtins.readFile ./sources.json);
+          packages = import ./. { inherit pkgs system; };
         in
-        rec {
-          zen-browser-unwrapped = pkgs.callPackage ./zen-browser-unwrapped.nix {
-            inherit (sources.${system}) hash url;
-            inherit (sources) version;
-          };
-          zen-browser = pkgs.callPackage ./zen-browser.nix { inherit zen-browser-unwrapped; };
-          zen-browser-generic = builtins.trace "WARNING: Zen upstream no longer differentiates between specific and generic builds, this package is kept for flake backwards-compatibility only. Please use the default `zen-browser` package instead." zen-browser;
-          default = zen-browser;
-        }
+        packages
       );
 
       apps = forAllSystems (
